7fda13be6fde819d88baffecd76cf902f9ddc3c2
HJY1029 <139771395+HJY1029@users.noreply.github.com>
2024-03-20 16:42:05 +0800

    Update GPAcalculator.java

--- src/grade/GPAcalculator.java
+++ src/grade/GPAcalculator.java
@@ -5,10 +5,10 @@ public class GPAcalculator {
 
         Course course1 = new Course("Math", 3, 85);
         Course course2 = new Course("English", 2, 78);
-
+        Course course3 = new Course("Physics",2,74);
         student.addCourse(course1);
         student.addCourse(course2);
-
+        student.addCourse(course3);
         student.displayStudentInfo();
 
         double totalGPA = student.calculateTotalGPA();
